c3c5ad61a7745cbc16adeedf3a805351f047bc8c
matrix-bro <razmhz11@gmail.com>
2024-01-28 13:32:23 +0545

    Initial Project Setup using Typescript

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <div className="w-full bg-gray-900">
+        <h1 className="text-center text-white text-3xl py-2">Wordle</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
